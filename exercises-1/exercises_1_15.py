10bc504b9285a747c7d50c6ac1160b013c794b02
rafaelrmadeira001 <mareirarte@gmail.com>
2025-10-01 14:27:07 -0300

    Update exercises_1_15.py

--- exercises-1/exercises_1_15.py
+++ exercises-1/exercises_1_15.py
@@ -2,6 +2,4 @@ produto = 'Livro'
 preco = 35.50
 desconto = 0.10
 preco_com_desconto = preco * (1 - desconto)
-
-print(f'O {produto} custa R${(preco} e, com {int(desconto*100)}% de desconto fica por R${preco_com_desconto}.')
-
+print(f'O {produto} custa R${preco:.2f} e, com {int(desconto * 100)}% de desconto fica por R${preco_com_desconto:.2f}.')
